d90f94a63a3452ab3b31acefdcac8dcce9026669
Kacper Tryniecki <kacper@tryniecki.com>
2019-03-08 15:38:28 +0100

    UWP better

---
+++ Client/mobiBooking.ClientApp/mobiBooking.UWP/Views/LoginPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace mobiBooking.UWP.Views
+{
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Button(object sejnder, RoutedEventArgs e)
+        {
+            Login obj = new Login
+            {
+                Email = email.Text,
+                Password = password.Text,
+            };
+            string json = JsonConvert.SerializeObject(obj);
+            Console.WriteLine(json);
+
+
+            RestClient client = new RestClient("http://192.168.10.240:51290/api");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+            client.RemoteCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+
+            RestRequest request = new RestRequest("Authenticate", Method.POST);
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            string content = response.Content; // raw content as string
+
+            ResponseClass responseObj = new ResponseClass();
+            responseObj = JsonConvert.DeserializeObject<ResponseClass>(response.Content);
+
+            if (responseObj.Token != null)
+            {
+                Frame.Navigate(typeof(MainPage), responseObj);
+            }
+        }
+    }
+
+    internal class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Client/mobiBooking.ClientApp/mobiBooking.UWP/Views/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using mobiBooking.UWP.Views;
+using Newtonsoft.Json;
+using RestSharp;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace mobiBooking.UWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CoreApplicationViewTitleBar CoreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            CoreTitleBar.ExtendViewIntoTitleBar = true;
+
+            Window.Current.SetTitleBar(DragArea);
+
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            ContentFrame.Navigate(typeof(DashboardPage));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var parameters = (ResponseClass)e.Parameter;
+        }
+
+        private void LogOut_Click(object sejnder, RoutedEventArgs e)
+        {
+            ResponseClass objResponse = new ResponseClass();
+            string json = JsonConvert.SerializeObject(objResponse.Token);
+
+
+            RestClient client = new RestClient("http://192.168.10.240:51290/api");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+            client.RemoteCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+
+            RestRequest request = new RestRequest("Authenticate/Logout", Method.POST);
+            request.AddParameter("Authorization", "Bearer " + objResponse.Token, ParameterType.HttpHeader);
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            string content = response.Content; // raw content as string
+
+            Frame.Navigate(typeof(LoginPage));
+        }
+    }
+}
