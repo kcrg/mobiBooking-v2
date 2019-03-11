e99756cc9393e49191c2c78f9d0b5e5092dbbd05
Kacper Tryniecki <kacper@tryniecki.com>
2019-03-11 14:27:54 +0100

    Added select all button in BookPage

--- Client/mobiBooking.ClientApp/mobiBooking.UWP/Views/BookPage.xaml.cs
+++ Client/mobiBooking.ClientApp/mobiBooking.UWP/Views/BookPage.xaml.cs
@@ -8,5 +8,17 @@ namespace mobiBooking.UWP.Views
         {
             InitializeComponent();
         }
+
+        private void SelectAll_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (SelectAll.IsChecked == true)
+            {
+                UsersList.SelectAll();
+            }
+            else
+            {
+                UsersList.SelectedIndex = -1;
+            }
+        }
     }
 }
